84c9ab2d54113e65a38853700e194fc4f1f1bcaa
Prady <pradyumnadoddala@gmail.com>
2015-10-26 03:00:25 -0500

    Fixed the bug to create socket. Now the data can be sent to Spark server.

--- iHRobo/iHRobo/DMRecognizerViewController.m
+++ iHRobo/iHRobo/DMRecognizerViewController.m
@@ -4,22 +4,22 @@
 //
 // Copyright 2010, Nuance Communications Inc. All rights reserved.
 //
-// Nuance Communications, Inc. provides this document without representation 
-// or warranty of any kind. The information in this document is subject to 
-// change without notice and does not represent a commitment by Nuance 
-// Communications, Inc. The software and/or databases described in this 
-// document are furnished under a license agreement and may be used or 
-// copied only in accordance with the terms of such license agreement.  
-// Without limiting the rights under copyright reserved herein, and except 
-// as permitted by such license agreement, no part of this document may be 
-// reproduced or transmitted in any form or by any means, including, without 
-// limitation, electronic, mechanical, photocopying, recording, or otherwise, 
-// or transferred to information storage and retrieval systems, without the 
+// Nuance Communications, Inc. provides this document without representation
+// or warranty of any kind. The information in this document is subject to
+// change without notice and does not represent a commitment by Nuance
+// Communications, Inc. The software and/or databases described in this
+// document are furnished under a license agreement and may be used or
+// copied only in accordance with the terms of such license agreement.
+// Without limiting the rights under copyright reserved herein, and except
+// as permitted by such license agreement, no part of this document may be
+// reproduced or transmitted in any form or by any means, including, without
+// limitation, electronic, mechanical, photocopying, recording, or otherwise,
+// or transferred to information storage and retrieval systems, without the
 // prior written permission of Nuance Communications, Inc.
-// 
-// Nuance, the Nuance logo, Nuance Recognizer, and Nuance Vocalizer are 
-// trademarks or registered trademarks of Nuance Communications, Inc. or its 
-// affiliates in the United States and/or other countries. All other 
+//
+// Nuance, the Nuance logo, Nuance Recognizer, and Nuance Vocalizer are
+// trademarks or registered trademarks of Nuance Communications, Inc. or its
+// affiliates in the United States and/or other countries. All other
 // trademarks referenced herein are the property of their respective owners.
 //
 
@@ -39,7 +39,7 @@
 const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0x18, 0xc3, 0x4e, 0xe1, 0x5a, 0xbe, 0xae, 0x85, 0xea, 0x7c, 0xcb, 0xec, 0x6a, 0x8e, 0x18, 0x07, 0xf9, 0x69, 0x6d, 0x5c, 0x8e, 0x06, 0x49, 0x40, 0x74, 0x26, 0x0e, 0x29, 0x71, 0x8d, 0xb4, 0x89, 0x50, 0x02, 0x6a, 0xaa, 0xc1, 0x19, 0x2f, 0xab, 0x95, 0xfa, 0x98, 0x00, 0x88, 0xb0, 0x07, 0x81, 0x19, 0x74, 0xdd, 0xa7, 0x7a, 0x27, 0xe8, 0xef, 0x4d, 0xf6, 0x18};
 
 
-@interface DMRecognizerViewController(){
+@interface DMRecognizerViewController() {
     
 }
 
@@ -62,23 +62,23 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     
     isRunning = NO;
     
-    NSLog(@"Ip Address is : %@", [self getIPAddress]);
-
-	
+    NSLog(@"IP Address : %@ : %d", [self getIPAddress], PORT);
+    
+    
     [SpeechKit setupWithID:@"NMDPTRIAL_pradyumnadoddala_gmail_com20151013142247"
                       host:@"sslsandbox.nmdp.nuancemobility.net"
                       port:443
                     useSSL:YES
                   delegate:nil];
     
-	// Set earcons to play
-	SKEarcon* earconStart	= [SKEarcon earconWithName:@"earcon_listening.wav"];
-	SKEarcon* earconStop	= [SKEarcon earconWithName:@"earcon_done_listening.wav"];
-	SKEarcon* earconCancel	= [SKEarcon earconWithName:@"earcon_cancel.wav"];
-	
-	[SpeechKit setEarcon:earconStart forType:SKStartRecordingEarconType];
-	[SpeechKit setEarcon:earconStop forType:SKStopRecordingEarconType];
-	[SpeechKit setEarcon:earconCancel forType:SKCancelRecordingEarconType];
+    // Set earcons to play
+    SKEarcon* earconStart	= [SKEarcon earconWithName:@"earcon_listening.wav"];
+    SKEarcon* earconStop	= [SKEarcon earconWithName:@"earcon_done_listening.wav"];
+    SKEarcon* earconCancel	= [SKEarcon earconWithName:@"earcon_cancel.wav"];
+    
+    [SpeechKit setEarcon:earconStart forType:SKStartRecordingEarconType];
+    [SpeechKit setEarcon:earconStop forType:SKStopRecordingEarconType];
+    [SpeechKit setEarcon:earconCancel forType:SKCancelRecordingEarconType];
     
     
     // For Textview
@@ -86,30 +86,31 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     [_textView setUserInteractionEnabled:YES];
     _textView.delegate =self;
     [self.view addSubview:_textView];
+    
+    [self toggleSocketState];
 }
 
 /*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 
 - (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
+    // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
+    
+    // Release any cached data, images, etc that aren't in use.
 }
 
 - (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
 }
 
-#pragma mark -
-#pragma mark - Text View delegates
+#pragma mark - Text View delegate
 
 -(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:
 (NSRange)range replacementText:(NSString *)text{
@@ -136,29 +137,28 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     return YES;
 }
 
- //reassigning the keyboard by touching on screens
+//reassigning the keyboard by touching on screens
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
     NSLog(@"touchesBegan:withEvent:");
     [self.view endEditing:YES];
     [super touchesBegan:touches withEvent:event];
 }
 
-#pragma mark - 
-#pragma mark Send to Spark button
+#pragma mark -
+#pragma mark Actions
 
 - (IBAction)SendtoSpark:(id)sender {
     
     NSString *msg = self.searchBox.text;
     NSLog(@"message is %@",msg);
     NSData *dataout = [msg dataUsingEncoding:NSASCIIStringEncoding];
-    [listenSocket writeData:dataout withTimeout:-1 tag:0];
+    GCDAsyncSocket *socket = [connectedSockets lastObject];
+    [socket writeData:dataout withTimeout:-1 tag:0];
+    [socket writeData:[@"\r\n" dataUsingEncoding:NSASCIIStringEncoding] withTimeout:-1 tag:0];
     //[listenSocket writeData:data  withTimeout:-1 tag:theTag];
     [self speakText:@"Sending data to spark"];
     
 }
-
-#pragma mark -
-#pragma mark Actions
 
 - (IBAction)recordButtonAction: (id)sender {
     [self.searchBox resignFirstResponder];
@@ -176,52 +176,52 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
         detectionType = SKShortEndOfSpeechDetection; /* Searches tend to be short utterances free of pauses. */
         recoType = SKSearchRecognizerType; /* Optimize recognition performance for search text. */
         
-//        if (recognitionType.selectedSegmentIndex == 0) {
-//            /* 'Search' is selected */
-//            detectionType = SKShortEndOfSpeechDetection; /* Searches tend to be short utterances free of pauses. */
-//            recoType = SKSearchRecognizerType; /* Optimize recognition performance for search text. */
-//        }
-//        else if (recognitionType.selectedSegmentIndex == 1){
-//            /* 'Dictation' is selected */
-//            detectionType = SKLongEndOfSpeechDetection; /* Dictations tend to be long utterances that may include short pauses. */
-//            recoType = SKDictationRecognizerType; /* Optimize recognition performance for dictation or message text. */
-//        } else {
-//            /* 'TV' is selected */
-//            detectionType = SKLongEndOfSpeechDetection; /* Dictations tend to be long utterances that may include short pauses. */
-//            recoType = SKTvRecognizerType; /* Optimize recognition performance for dictation or message text. */
-//        }
-		
+        //        if (recognitionType.selectedSegmentIndex == 0) {
+        //            /* 'Search' is selected */
+        //            detectionType = SKShortEndOfSpeechDetection; /* Searches tend to be short utterances free of pauses. */
+        //            recoType = SKSearchRecognizerType; /* Optimize recognition performance for search text. */
+        //        }
+        //        else if (recognitionType.selectedSegmentIndex == 1){
+        //            /* 'Dictation' is selected */
+        //            detectionType = SKLongEndOfSpeechDetection; /* Dictations tend to be long utterances that may include short pauses. */
+        //            recoType = SKDictationRecognizerType; /* Optimize recognition performance for dictation or message text. */
+        //        } else {
+        //            /* 'TV' is selected */
+        //            detectionType = SKLongEndOfSpeechDetection; /* Dictations tend to be long utterances that may include short pauses. */
+        //            recoType = SKTvRecognizerType; /* Optimize recognition performance for dictation or message text. */
+        //        }
+        
         langType = @"en_US";
         
-//		switch (languageType.selectedSegmentIndex) {
-//			case 0:
-//				langType = @"en_US";
-//				break;
-//			case 1:
-//				langType = @"en_GB";
-//				break;
-//			case 2:
-//				langType = @"fr_FR";
-//				break;
-//			case 3:
-//				langType = @"de_DE";
-//				break;
-//			default:
-//				langType = @"en_US";
-//				break;
-//		}
+        //		switch (languageType.selectedSegmentIndex) {
+        //			case 0:
+        //				langType = @"en_US";
+        //				break;
+        //			case 1:
+        //				langType = @"en_GB";
+        //				break;
+        //			case 2:
+        //				langType = @"fr_FR";
+        //				break;
+        //			case 3:
+        //				langType = @"de_DE";
+        //				break;
+        //			default:
+        //				langType = @"en_US";
+        //				break;
+        //		}
         /* Nuance can also create a custom recognition type optimized for your application if neither search nor dictation are appropriate. */
         
         NSLog(@"Recognizing type:'%@' Language Code: '%@' using end-of-speech detection:%d.", recoType, langType, detectionType);
-
+        
         if (_voiceSearch) {
             _voiceSearch = nil;
         }
-		
+        
         _voiceSearch = [[SKRecognizer alloc] initWithType:recoType
-                                               detection:detectionType
-                                                language:langType 
-                                                delegate:self];
+                                                detection:detectionType
+                                                 language:langType
+                                                 delegate:self];
     }
 }
 
@@ -240,7 +240,7 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
 - (void)updateVUMeter {
     float width = (90+_voiceSearch.audioLevel)*5/2;
     
-    [self setVUMeterWidth:width];    
+    [self setVUMeterWidth:width];
     [self performSelector:@selector(updateVUMeter) withObject:nil afterDelay:0.05];
 }
 
@@ -262,14 +262,14 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     //                useSSL:NO
     //              delegate:self];
     //
-	// Set earcons to play
-	//SKEarcon* earconStart	= [SKEarcon earconWithName:@"earcon_listening.wav"];
-	//SKEarcon* earconStop	= [SKEarcon earconWithName:@"earcon_done_listening.wav"];
-	//SKEarcon* earconCancel	= [SKEarcon earconWithName:@"earcon_cancel.wav"];
-	//
-	//[SpeechKit setEarcon:earconStart forType:SKStartRecordingEarconType];
-	//[SpeechKit setEarcon:earconStop forType:SKStopRecordingEarconType];
-	//[SpeechKit setEarcon:earconCancel forType:SKCancelRecordingEarconType];    
+    // Set earcons to play
+    //SKEarcon* earconStart	= [SKEarcon earconWithName:@"earcon_listening.wav"];
+    //SKEarcon* earconStop	= [SKEarcon earconWithName:@"earcon_done_listening.wav"];
+    //SKEarcon* earconCancel	= [SKEarcon earconWithName:@"earcon_cancel.wav"];
+    //
+    //[SpeechKit setEarcon:earconStart forType:SKStartRecordingEarconType];
+    //[SpeechKit setEarcon:earconStop forType:SKStopRecordingEarconType];
+    //[SpeechKit setEarcon:earconCancel forType:SKCancelRecordingEarconType];
 }
 
 #pragma mark -
@@ -287,7 +287,7 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
 - (void)recognizerDidFinishRecording:(SKRecognizer *)recognizer
 {
     NSLog(@"Recording finished.");
-
+    
     [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(updateVUMeter) object:nil];
     [self setVUMeterWidth:0.];
     transactionState = TS_PROCESSING;
@@ -297,8 +297,8 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
 - (void)recognizer:(SKRecognizer *)recognizer didFinishWithResults:(SKRecognition *)results
 {
     NSLog(@"Got results.");
-    NSLog(@"Session id [%@].", [SpeechKit sessionID]); // for debugging purpose: printing out the speechkit session id 
-
+    NSLog(@"Session id [%@].", [SpeechKit sessionID]); // for debugging purpose: printing out the speechkit session id
+    
     long numOfResults = [results.results count];
     
     transactionState = TS_IDLE;
@@ -307,8 +307,8 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     if (numOfResults > 0)
         self.searchBox.text = [results firstResult];
     
-	if (numOfResults > 1) 
-		NSLog(@"%@", [[results.results subarrayWithRange:NSMakeRange(1, numOfResults-1)] componentsJoinedByString:@"\n"]);
+    if (numOfResults > 1)
+        NSLog(@"%@", [[results.results subarrayWithRange:NSMakeRange(1, numOfResults-1)] componentsJoinedByString:@"\n"]);
     
     if (results.suggestion){
         UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Suggestion"
@@ -322,14 +322,14 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
         [self presentViewController:alert animated:YES completion:nil];
         
     }
-
-	_voiceSearch = nil;
+    
+    _voiceSearch = nil;
 }
 
 - (void)recognizer:(SKRecognizer *)recognizer didFinishWithError:(NSError *)error suggestion:(NSString *)suggestion
 {
     NSLog(@"Got error.");
-    NSLog(@"Session id [%@].", [SpeechKit sessionID]); // for debugging purpose: printing out the speechkit session id 
+    NSLog(@"Session id [%@].", [SpeechKit sessionID]); // for debugging purpose: printing out the speechkit session id
     
     transactionState = TS_IDLE;
     [self.recordButton setTitle:@"Record" forState:UIControlStateNormal];
@@ -358,11 +358,11 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
         
     }
     
-	_voiceSearch = nil;
+    _voiceSearch = nil;
 }
 
 #pragma mark -
-#pragma mark UITextFieldDelegate methods
+#pragma mark UITextField Delegate
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
@@ -375,7 +375,7 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
 
 
 #pragma mark -
-#pragma mark Ipaddress
+#pragma mark IP Address
 
 - (NSString *)getIPAddress
 {
@@ -407,9 +407,7 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     return address;
 }
 
-
-#pragma mark -
-#pragma mark Socket
+#pragma mark - Socket
 
 - (void)toggleSocketState
 {
@@ -448,6 +446,37 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     }
 }
 
+- (void)socket:(GCDAsyncSocket *)sock didAcceptNewSocket:(GCDAsyncSocket *)newSocket
+{
+    // This method is executed on the socketQueue (not the main thread)
+    
+    @synchronized(connectedSockets)
+    {
+        [connectedSockets addObject:newSocket];
+    }
+    
+    NSString *host = [newSocket connectedHost];
+    UInt16 port = [newSocket connectedPort];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        @autoreleasepool {
+            
+            [self log:FORMAT(@"Accepted client %@:%hu", host, port)];
+            
+        }
+    });
+    
+    NSString *welcomeMsg = @"Welcome to the iPhone Server\r\n";
+    NSData *welcomeData = [welcomeMsg dataUsingEncoding:NSUTF8StringEncoding];
+    
+    [newSocket writeData:welcomeData withTimeout:-1 tag:0];
+    
+    
+    [newSocket readDataWithTimeout:READ_TIMEOUT tag:0];
+    newSocket.delegate = self;
+    
+    //    [newSocket readDataToData:[GCDAsyncSocket CRLFData] withTimeout:READ_TIMEOUT tag:0];
+}
 
 - (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag
 {
@@ -467,8 +496,11 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     NSString *msg = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     
     [self log:msg];
-    _textView.text = msg;
-    //[self speakText:msg];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        _textView.text = msg;
+    });
+    
+    [self speakText:msg];
     [sock readDataWithTimeout:READ_TIMEOUT tag:0];
 }
 
@@ -511,21 +543,19 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     }
 }
 
+#pragma mark - Helpers
+
 - (void)log:(NSString *)msg {
     NSLog(@"%@", msg);
 }
 
 - (NSString *)direction:(NSString *)message {
-    
     return @"";
 }
 
-
-#pragma mark -
-#pragma mark -text to speech function
+#pragma mark - TTS
 
 - (void)speakText:(NSString*)text {
-    NSLog(@"in speakText");
     NSString *cmd = [text uppercaseString];
     if (_synthesizer == nil) {
         _synthesizer = [[AVSpeechSynthesizer alloc] init];
@@ -533,14 +563,10 @@ const unsigned char SpeechKitApplicationKey[] = {0xcd, 0xb5, 0x4a, 0x7f, 0x8b, 0
     }
     
     AVSpeechUtterance *utterence = [[AVSpeechUtterance alloc] initWithString:cmd];
-    utterence.rate = _speed;
-    
     AVSpeechSynthesisVoice *voice = [AVSpeechSynthesisVoice voiceWithLanguage:@"en-US"];
     [utterence setVoice:voice];
     
     [_synthesizer speakUtterance:utterence];
-    
 }
-
 
 @end
